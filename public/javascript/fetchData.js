655585fdde25508559e95eca588c12e444ea5f65
Moncef-Tighi <monceftighiouart@gmail.com>
2022-01-13 19:22:27 +0100

    Ajout de la notation en étoile

--- public/javascript/fetchData.js
+++ public/javascript/fetchData.js
@@ -7,8 +7,29 @@ const classement= document.querySelector("#classement");
 const classementContainer = document.querySelector("#classementContainer");
 
 
+/*
+    Les images des étoiles sont en SVG, c'est plus simple de les passer via JS
+    Même si du coup ça fait un code bizarre
+*/
+const fullStar=`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>`
+const halfStar=`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 5.173l2.335 4.817 5.305.732-3.861 3.71.942 5.27-4.721-2.524v-12.005zm0-4.586l-3.668 7.568-8.332 1.151 6.064 5.828-1.48 8.279 7.416-3.967 7.416 3.966-1.48-8.279 6.064-5.827-8.332-1.15-3.668-7.569z"/></svg>`
+const emptyStar=`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 5.173l2.335 4.817 5.305.732-3.861 3.71.942 5.27-4.721-2.524-4.721 2.525.942-5.27-3.861-3.71 5.305-.733 2.335-4.817zm0-4.586l-3.668 7.568-8.332 1.151 6.064 5.828-1.48 8.279 7.416-3.967 7.416 3.966-1.48-8.279 6.064-5.827-8.332-1.15-3.668-7.569z"/></svg>`
+
 function fetchThenjson(link) {
     return fetch(link).then(response => response.json())
+}
+
+const getStars = function(rating){
+    let i=1;
+    let output=""
+    while (i<=5) {
+        
+        if (i<=rating) output+=fullStar
+        else if (i-0.5<=rating) output+=halfStar
+        else output+=emptyStar
+        i+=1;
+    }
+    return output
 }
 
 const previousIcon= document.querySelectorAll(".previous");
@@ -33,7 +54,7 @@ const accueilDisplay = async () => {
             <h2>${book.title}</h2>
             <img src="${book.img}" class="acceuil_illustration">
             <h3>${book.author.split(",")[0]}</h3>
-            <span>${book.rating} (${book.totalratings})</span>
+            <span>${getStars(book.rating)}${book.rating} (${book.totalratings})</span>
         </div>
         `
         accueilContainer.insertAdjacentHTML("beforeend", html)
@@ -61,7 +82,7 @@ const fetchClassement= async function(type,tag) {
                 <img src="${book.img}" class="classement_illustration">
                 <h2>${book.title}</h2>
                 <h3>By - ${book.author.split(",")[0]}</h3>
-                <span>${book.rating} (${book.totalratings})</span>
+                <span>${getStars(book.rating)}${book.rating} (${book.totalratings})</span>
                 <ul class="tagList">
                     <li class="tags ${tags[0]}">${tags[0]}</li>
                     <li class="tags ${tags[1]}">${tags[1]}</li>
