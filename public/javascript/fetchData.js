a71d7fd31ffcafc62967ad70d6c7be5909098401
Moncef-Tighi <monceftighiouart@gmail.com>
2022-01-13 14:32:21 +0100

    première version du design de la section classement

--- public/javascript/fetchData.js
+++ public/javascript/fetchData.js
@@ -3,19 +3,22 @@ const accueilContainer= document.querySelector("#featured_books")
 const linkBest = document.querySelectorAll(".best");
 const linkTop = document.querySelectorAll(".top")
 const accueil = document.querySelector("#accueil");
-const classement= document.querySelector("#classement")
-const previousIcon= document.querySelector(".previous");
+const classement= document.querySelector("#classement");
+const classementContainer = document.querySelector("#classementContainer");
 
 
 function fetchThenjson(link) {
     return fetch(link).then(response => response.json())
 }
 
-previousIcon.forEach(icon=> icon.addEventListener("click", (event) => {
-    event.preventDefault();
-    classement.style.display="none";
-    accueil.style.display="none";
-}) );
+const previousIcon= document.querySelectorAll(".previous");
+previousIcon.forEach(icon=> {icon.addEventListener("click", (event) => {
+        console.log(event.target);
+        event.preventDefault();
+        classement.style.display="none";
+        accueil.style.display="block";
+    })
+});
 
 
 /*
@@ -46,18 +49,20 @@ const fetchClassement= async function(type,tag) {
     const data = await fetchThenjson(`${url}/books/${type}-100/${tag}`);
     if (data) {
         accueil.style.display="none";
-        classement.style.display="grid";
-        let html=`
-        <div>
-            <h2>${book.title}</h2>
-            <img src="${book.img}" class="acceuil_illustration">
-            <h3>${book.author.split(",")[0]}</h3>
-            <span>${book.rating} (${book.totalratings})</span>
-        </div>
-        `
-        classementContainer.insertAdjacentHTML("beforeend", html)
+        classement.style.display="block";
+        
+        data.books.forEach(book=>{
+            let html=`
+            <div>
+                <h2>${book.title}</h2>
+                <img src="${book.img}" class="classement_illustration">
+                <h3>${book.author.split(",")[0]}</h3>
+                <span>${book.rating} (${book.totalratings})</span>
+            </div>
+            `
+            classementContainer.insertAdjacentHTML("beforeend", html);
+        })       
     }
-    console.log(data);
 }
 
 const linkController = function(event) {
